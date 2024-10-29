280ff2f46c7d0671ab67fdf424d21a6ea57fa455
devtengry <imamogluomer@outlook.com>
2024-10-29 23:07:31 +0300

    EndpointRest class deleted

--- lib/main.dart
+++ lib/main.dart
@@ -27,10 +27,3 @@ class _TestClassState extends State<TestClass> {
   }
 }
 
-/* class EndpointRest extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-} */
