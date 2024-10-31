54210d57595a5d0c7360e12fff5a809328bca0d5
devtengry <imamogluomer@outlook.com>
2024-10-31 22:03:37 +0300

    MaterialApp adjusted

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      home: const TestClass(),
+      theme: ThemeData.dark(),
+    );
   }
 }
 
