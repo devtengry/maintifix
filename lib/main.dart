905d0ed106961bf53c6457508c2afaae9ebb9720
devtengry <imamogluomer@outlook.com>
2024-11-07 23:39:23 +0300

    test stateful widget created

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,17 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
