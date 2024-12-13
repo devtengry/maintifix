ec1d5bb75824e4f280090813eb1c78264b997454
devtengry <imamogluomer@outlook.com>
2024-12-13 18:53:25 +0300

    .env setted

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -52,12 +53,12 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDN0YHFm86AKSQLdOiN25sDBOncyB43k_o',
-    appId: '1:818650449509:android:daea3fb3be5b9f4293688c',
-    messagingSenderId: '818650449509',
-    projectId: 'maintifix-auth',
-    storageBucket: 'maintifix-auth.firebasestorage.app',
+  static FirebaseOptions android = FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY']!,
+    appId: dotenv.env['FIREBASE_APP_ID']!,
+    messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+    storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
   );
 }
 //asdasdsa
